7a076d377f4719c2e3de1b984afdce2aa2df50e1
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2020-01-26 01:39:36 +0000

    apply fixes from pg-error-snippet

--- test/postgres-error-output.js
+++ test/postgres-error-output.js
@@ -1,18 +1,20 @@
-const path = require('path')
-const test = require('ava')
-const {Pool} = require('pg')
-const {sqlModules} = require('../src')
-const {postgresErrorOutput} = require('../src/sql-modules')
+const path = require("path");
+const test = require("ava");
+const { Pool } = require("pg");
+const { sqlModules } = require("../src");
+const { postgresErrorOutput } = require("../src/sql-modules");
 
-const pool = new Pool({ connectionString: 'postgres://melty:puffs@localhost:5435/pg_error_snippet' })
+const pool = new Pool({
+  connectionString: "postgres://postgres:postgres@localhost:5432/sql_modules"
+});
 
-test('formats postgres error', async (t) => {
-  const dir = path.join(__dirname, 'fixtures/syntaxError')
-  const {files, query} = await sqlModules(dir)
+test("formats postgres error", async t => {
+  const dir = path.join(__dirname, "fixtures/syntaxError");
+  const { files, query } = await sqlModules(dir);
   try {
-    await pool.query(query)
-  } catch(err) {
-    const output = postgresErrorOutput(err, query, files)
-    t.snapshot(output)
+    await pool.query(query);
+  } catch (err) {
+    const output = postgresErrorOutput(err, query, files);
+    t.snapshot(output);
   }
-})
+});
