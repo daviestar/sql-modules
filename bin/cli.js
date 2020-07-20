148d2bf2293824d1b85ea6193f6b71738d8ddb3e
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2020-07-21 00:57:22 +0100

    cli improvements

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
+const path = require('path')
 const fs = require('fs')
 const { promisify } = require('util')
 const parseArgs = require('minimist')
-const { sqlModules } = require('../index')
+const { joinFiles } = require('topo-files')
+const { sqlModules } = require('../src/index')
 const writeFile = promisify(fs.writeFile)
 
 async function run(directory, options) {
   try {
-    const output = await sqlModules(directory, options)
+    const { files } = await sqlModules(directory, options)
+    // console.log(files)
+    const output = joinFiles(files)
+    console.log(output)
     if (options.outFile) {
       await writeFileToDisk(options.outFile, output)
     } else {
       process.stdout.write(output)
     }
     process.exit(0)
-  } catch (err) { }
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 async function writeFileToDisk(filePath, output) {
   try {
-    await writeFile(filePath, output)
+    await writeFile(path.join(process.cwd(), filePath), output)
   } catch (err) {
     console.error(`[topo-files]: Error writing to file "${filePath}" ${err.message}`)
   }
